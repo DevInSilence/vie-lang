66f7eae3226de3f3251ef0367cb56770873992a0
Fabrice <fabrice.schaub@swisscom.com>
2024-10-01 21:04:44 +0200

    gute frage

--- lib/vie_parser/lexer.ml
+++ lib/vie_parser/lexer.ml
@@ -4,8 +4,7 @@ module Token = Vie_lexer.Token
 open Camomile
 module UTF8 = Camomile.UTF8
 
-
-(*Utility functions*)
+(* Utility functions *)
 
 let update_pos line_ref column_ref char =
   if UChar.code char = (UChar.code (UChar.of_char '\n')) then (
@@ -20,30 +19,38 @@ let peek_char source idx =
   if idx >= UTF8.length source then None
   else Some (UTF8.get source idx)
 
-  let collect source start_idx idx : string =
-    Vie_common.Unicode.utf8_sub source start_idx (idx - start_idx)  
-
-let is_valid_number_start c = c >= '0' && c <= '9'
-
-let is_valid_identifier_start c =
-  (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c = '_'
-
-let is_valid_identifier_continuation c =
-  is_valid_identifier_start c || is_valid_number_start c
+let collect source start_idx idx : string =
+  Vie_common.Unicode.utf8_sub source start_idx (idx - start_idx)
 
 let classify_identifier iden loc: Token.t =
   match iden with
   | _ -> Token.Iden (iden, loc)
 
-(*Lexers*)
+let is_valid_number_start c =
+  let code = UChar.code c in
+  code >= UChar.code (UChar.of_char '0') && code <= UChar.code (UChar.of_char '9')
 
-let lex_number source start_idx idx_ref line_ref column_ref :
-    (Token.t, string) Result.t =
+let is_valid_identifier_start c =
+  let code = UChar.code c in
+  (code >= UChar.code (UChar.of_char 'a') && code <= UChar.code (UChar.of_char 'z')) ||
+  (code >= UChar.code (UChar.of_char 'A') && code <= UChar.code (UChar.of_char 'Z')) || 
+  code = UChar.code (UChar.of_char '_')
+
+let is_valid_identifier_continuation c =
+  is_valid_identifier_start c || is_valid_number_start c
+
+let is_whitespace c =
+  let code = UChar.code c in
+  code = UChar.code (UChar.of_char ' ') || code = UChar.code (UChar.of_char '\n') || code = UChar.code (UChar.of_char '\t')
+
+(* Lexer functions *)
+
+let lex_number source start_idx idx_ref line_ref column_ref : (Token.t, string) Result.t =
   let is_float = ref false in
   let rec collect_number () =
     match peek_char source !idx_ref with
-    | Some '.' when not !is_float ->
-        update_pos line_ref column_ref '.';
+    | Some c when UChar.code c = UChar.code (UChar.of_char '.') && not !is_float ->
+        update_pos line_ref column_ref c;
         idx_ref := !idx_ref + 1;
         is_float := true;
         collect_number ()
@@ -51,96 +58,84 @@ let lex_number source start_idx idx_ref line_ref column_ref :
         update_pos line_ref column_ref c;
         idx_ref := !idx_ref + 1;
         collect_number ()
-    | _ -> (
+    | _ -> 
         let num_str = collect source start_idx !idx_ref in
-        let loc =
-          Location.new_location start_idx !idx_ref !column_ref !line_ref
-        in
+        let loc = Location.new_location start_idx !idx_ref !column_ref !line_ref in
         if !is_float then
           try Result.Ok (Token.Decimal (float_of_string num_str, loc))
           with Failure _ -> Result.Error "Invalid float format"
         else
           try Result.Ok (Token.Number (int_of_string num_str, loc))
-          with Failure _ -> Result.Error "Invalid integer format")
+          with Failure _ -> Result.Error "Invalid integer format"
   in
   collect_number ()
 
-  let lex_identifiers source start_idx idx_ref line_ref column_ref :
-  (Token.t, string) Result.t =
-let rec collect_identifier () =
-  match peek_char source !idx_ref with
-  | Some c when is_valid_identifier_continuation c ->
-      update_pos line_ref column_ref c;
-      idx_ref := !idx_ref + 1;
-      collect_identifier ()
-  | _ ->
-      let iden = collect source start_idx !idx_ref in
-      let loc =
-        Location.new_location start_idx !idx_ref !column_ref !line_ref
-      in
-      Result.Ok (classify_identifier iden loc)
-in
-collect_identifier ()
+let lex_identifiers source start_idx idx_ref line_ref column_ref : (Token.t, string) Result.t =
+  let rec collect_identifier () =
+    match peek_char source !idx_ref with
+    | Some c when is_valid_identifier_continuation c ->
+        update_pos line_ref column_ref c;
+        idx_ref := !idx_ref + 1;
+        collect_identifier ()
+    | _ ->
+        let iden = collect source start_idx !idx_ref in
+        let loc = Location.new_location start_idx !idx_ref !column_ref !line_ref in
+        Result.Ok (classify_identifier iden loc)
+  in
+  collect_identifier ()
 
-(*Main lexer function*)
+(* Main lexer function *)
 
 let lex source : (Token.t list, string list) Result.t =
   let len = String.length source in
-
   let idx = ref 0 in
   let line = ref 1 in
   let column = ref 1 in
-
-  let tokens = ref ([] : Token.t list) in
-  let errs = ref ([] : string list) in
+  let tokens = ref [] in
+  let errs = ref [] in
 
   let rec lex_token () =
     if !idx >= len then
-      append_token tokens
-        (Token.EOF (Location.new_location !idx !idx !column !line))
+      append_token tokens (Token.EOF (Location.new_location !idx !idx !column !line))
     else
-      let char = source.[!idx] in
-      match char with
-      | ' ' | '\n' | '\t' ->
-          update_pos line column char;
+      match peek_char source !idx with
+      | Some c when is_whitespace c ->
+          update_pos line column c;
           idx := !idx + 1;
           lex_token ()
-      | '0' .. '9' ->
+      | Some c when is_valid_number_start c ->
           let start_idx = !idx in
           (match lex_number source start_idx idx line column with
           | Result.Ok token -> append_token tokens token
           | Result.Error err -> errs := err :: !errs);
           lex_token ()
-      | '.' -> (
-          match peek_char source (!idx + 1) with
-          | Some c when is_valid_number_start c ->
+      | Some c when UChar.code c = UChar.code (UChar.of_char '.') ->
+          (match peek_char source (!idx + 1) with
+          | Some next_c when is_valid_number_start next_c ->
               let start_idx = !idx in
               (match lex_number source start_idx idx line column with
               | Result.Ok token -> append_token tokens token
               | Result.Error err -> errs := err :: !errs);
               lex_token ()
           | _ ->
-              errs :=
-                Format.sprintf "Unexpected character '.' at line %d, column %d"
-                  !line !column
-                :: !errs;
+              errs := Format.sprintf "Unexpected character '.' at line %d, column %d" !line !column :: !errs;
               idx := !idx + 1;
               lex_token ())
-      | 'a' .. 'z' | 'A' .. 'Z' | '_' ->
+      | Some c when is_valid_identifier_start c ->
           let start_idx = !idx in
           (match lex_identifiers source start_idx idx line column with
           | Result.Ok token -> append_token tokens token
           | Result.Error err -> errs := err :: !errs);
           lex_token ()
-      | _ ->
-          errs :=
-            Format.sprintf "Unexpected character '%c' at line %d, column %d"
-              char !line !column
-            :: !errs;
+      | Some c ->
+          errs := Format.sprintf "Unexpected character '%s' at line %d, column %d"
+                                  (Vie_common.Unicode.uchar_to_string c)
+                                  !line !column :: !errs;
           idx := !idx + 1;
           lex_token ()
+      | None -> ()
   in
-  lex_token ();
 
+  lex_token ();
   if List.length !errs > 0 then Result.Error (List.rev !errs)
   else Result.Ok (List.rev !tokens)
--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,8 @@ open Vie_parser.Lexer
 open Vie_common.Result
 
 let () =
-  let tok_stream = lex "MaybeThiIsAVariablEName123 \n31.2.2" in
+  (* Test string with identifiers, floats, integers, and Unicode symbols *)
+  let tok_stream = lex "MaybeThiIsAVariablEName123\n31.2.2\na" in
   match result_map (List.map Token.format) tok_stream with
   | Ok formatted_tokens -> List.iter print_endline formatted_tokens
   | Error errs -> List.iter print_endline errs
