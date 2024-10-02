8c0397056441d6851fec89789ccec003d4149d57
Fabrice <fabrice.schaub@swisscom.com>
2024-10-02 17:32:12 +0200

    test

--- vie_compiler/main.ml
+++ vie_compiler/main.ml
@@ -1,4 +1,6 @@
 open Vie_parser
+open Vie_parser.Parser
+open Lexing
 
 let () =
   let source = "123" in
@@ -6,5 +8,7 @@ let () =
   match lex_res with
   | Ok tokens ->
       List.iter (fun t -> Printf.printf "%s\n" (Token.show t)) tokens;
+      let program = Parser.program Lexer.lex lex_res in
+
   | Error errs ->
       List.iter (fun e -> Printf.printf "Error: %s\n" e) errs;
