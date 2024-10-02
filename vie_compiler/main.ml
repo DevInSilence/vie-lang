63f71c5c51d0db126ac9dfaa338ac3e6841bb9a9
Fabrice <fabrice.schaub@swisscom.com>
2024-10-02 09:22:59 +0200

    created more distinct subprojects

--- vie_compiler/main.ml
+++ vie_compiler/main.ml
@@ -2,7 +2,6 @@ open Vie_parser.Lexer
 open Vie_common.Result
 
 let () =
-  (* Test string with identifiers, floats, integers, and Unicode symbols *)
   let tok_stream = lex "varNmae = 123.456 + 789.012" in
   match result_map (List.map Token.format) tok_stream with
   | Ok formatted_tokens -> List.iter print_endline formatted_tokens
